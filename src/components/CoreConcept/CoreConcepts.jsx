68f179021a0d6ad66a13599ae48f5ac6ec79601e
Mateusz <145119060+Lifyo@users.noreply.github.com>
2025-03-27 12:46:53 +0100

    Update CoreConcepts.jsx

--- src/components/CoreConcept/CoreConcepts.jsx
+++ src/components/CoreConcept/CoreConcepts.jsx
@@ -1,1 +1,29 @@
+import CoreConcept from './CoreConcept.jsx';
+import { CORE_CONCEPTS } from '../../data.js';
 
+export default function CoreConcepts() {
+	return (
+		<section id='core-concepts'>
+			<h2>Core Concepts</h2>
+			<ul>
+				{/* Dymaniczna wersja, nie musimy szukać po indeksie */}
+
+				{CORE_CONCEPTS.map((conceptItem) => (
+					<CoreConcept key={conceptItem.title} {...conceptItem} />
+				))}
+
+				{/* PEŁNA, DŁUŻSZA WERSJA (MNIEJ OPTYMALNA)
+                        <CoreConcept
+                            title={CORE_CONCEPTS[0].title}
+                            description={CORE_CONCEPTS[0].description}
+                            image={CORE_CONCEPTS[0].image}
+                        />
+
+                        SKRÓCONA, OPTYMALNA WERSJA
+                        <CoreConcept {...CORE_CONCEPTS[1]} />
+                        <CoreConcept {...CORE_CONCEPTS[2]} />
+                        <CoreConcept {...CORE_CONCEPTS[3]} /> */}
+			</ul>
+		</section>
+	);
+}
